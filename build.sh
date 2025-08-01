72bd8aee3047af4601b1336a6819c810eecb476a
Bantoniof <bantoniof@libreoffice.org>
2025-08-01 19:21:22 +0200

    correccion refs pt

--- build.sh
+++ build.sh
@@ -1,6 +1,32 @@
-#!/bin/sh
+#!/bin/bash
+ 
+# This file update the information of versions in description.xml
+# and  the update information.xml
+# and build (compress) the extension
 
-cd Chap2html
-zip -r Chap2html.oxt .
-cd ..
-mv Chap2html/Chap2html.oxt ./
+Ext="Chap2html"
+RotExt="Chap2html"
+
+if [[ ! $1 ]]
+then
+	echo "Usage:"
+	echo "./build.sh [version]"
+	echo "example: ./build.sh 12.2.3"
+	exit
+	else
+	echo
+    echo "### updating " $RotExt"/description.xml"
+   cd $RotExt
+	sed -i s'/<version value.*/<version value="'$1'"\/>/' description.xml
+    echo "### Compiling " $Ext
+    # I use 7zip but you can use zip:
+    # zip -r SanityCheck.oxt .
+     7z a ./$Ext.oxt . -tzip
+     cd ..
+    mv $RotExt/$Ext.oxt ./
+    echo 
+    echo "### updating " $Ext".update.xml"
+	sed -i s'/<version value.*/<version value="'$1'"\/>/' $Ext.update.xml
+	sed -i s'/raw.*/raw\/'$1'\/'$Ext'.oxt"\/>/' $Ext.update.xml
+    echo "### Done"
+fi
